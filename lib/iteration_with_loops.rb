24493a741dbf7d485d8ab15572d246e55fa0c67b
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-25 02:16:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,10 +3,12 @@ row_index=0
 while row_index<src.count do
   element_index=0
   while element_index<src[row_index].count do
-    src[row_index][element_index]
+    if src[row_index][element_index].is_a?(String)==true
+      joined_string += " " +src [row_index][element_index]
+    end
     element_index+=1
   end
   row_index+=1
 end
-src.join
+joined_string
 end
